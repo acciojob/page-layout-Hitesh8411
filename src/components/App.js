7b8f480e297f7ddd6cf4e6405108a339e4d545e0
Hitesh Suryavanshi <167613770+Hitesh8411@users.noreply.github.com>
2026-01-29 11:50:25 +0530

    Refactor App component to include PageLayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <PageLayout
+        header={<h1>Welcome to my website</h1>}
+        footer={<footer>© 2023 My Website. All rights reserved.</footer>}
+      >
+        <p>This is the content of my website</p>
+      </PageLayout>
+
+    </div>
+  );
+};
+
+export default App;
